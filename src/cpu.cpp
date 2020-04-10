46b106b64a1404d8b987129433f6436c4ea02132
flyingmat <paramattimatteo@gmail.com>
2020-04-10 21:58:50 +0100

    More opcodes and functions implemented

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -39,74 +39,86 @@ namespace GBEMU {
             this->F &= ((uint8_t) flag ^ 0xFF);
     }
 
+    void Cpu::setFlags(uint8_t flags) {
+        this->F |= flags;
+    }
+
     void Cpu::resetFlags(uint8_t flags) {
         this->F &= (flags ^ 0xFF);
     }
 
-    void Cpu::inc8bRegister(uint8_t& r) {
+    void Cpu::inc8b(uint8_t& r) {
         r++;
         this->setFlag(Flag::zf, r == 0x00);
         this->setFlag(Flag::n, 0);
         this->setFlag(Flag::h, r == 0x10);
     }
 
-    void Cpu::dec8bRegister(uint8_t& r) {
+    void Cpu::dec8b(uint8_t& r) {
         r--;
         this->setFlag(Flag::zf, r == 0xFF);
         this->setFlag(Flag::n, 1);
         this->setFlag(Flag::h, r == 0x0F);
     }
 
-    void Cpu::rl8bRegister(uint8_t& r) {
+    void Cpu::rl8b(uint8_t& r) {
         bool msb = r >> 7;
         r = ((r << 1) & 0xFF) | this->getFlag(Flag::cy);
         this->setFlag(Flag::cy, msb);
         this->resetFlags((uint8_t) Flag::zf | (uint8_t) Flag::n | (uint8_t) Flag::h);
     }
 
-    void Cpu::rr8bRegister(uint8_t& r) {
+    void Cpu::rr8b(uint8_t& r) {
         bool lsb = r & 0x01;
         r = (r >> 1) | (this->getFlag(Flag::cy) << 7);
         this->setFlag(Flag::cy, lsb);
         this->resetFlags((uint8_t) Flag::zf | (uint8_t) Flag::n | (uint8_t) Flag::h);
     }
 
-    void Cpu::rlc8bRegister(uint8_t& r) {
+    void Cpu::rlc8b(uint8_t& r) {
         this->setFlag(Flag::cy, r >> 7);
         r = ((r << 1) & 0xFF) | (r >> 7);
         this->resetFlags((uint8_t) Flag::zf | (uint8_t) Flag::n | (uint8_t) Flag::h);
     }
 
-    void Cpu::rrc8bRegister(uint8_t& r) {
+    void Cpu::rrc8b(uint8_t& r) {
         this->setFlag(Flag::cy, r & 0x01);
         r = (r >> 1) | ((r & 0x01) << 7);
         this->resetFlags((uint8_t) Flag::zf | (uint8_t) Flag::n | (uint8_t) Flag::h);
     }
 
-    void Cpu::inc16bRegister(uint8_t& r1, uint8_t& r2) {
+    void Cpu::inc16b2(uint8_t& r1, uint8_t& r2) {
         r2++;
         if (!r2)
             r1++;
     }
 
-    void Cpu::dec16bRegister(uint8_t& r1, uint8_t& r2) {
+    void Cpu::dec16b2(uint8_t& r1, uint8_t& r2) {
         r2--;
         if (r2 == 0xFF)
             r1--;
     }
 
-    void Cpu::mov16bRegister(uint16_t addr, uint16_t value) {
+    void Cpu::mov16bmem(uint16_t addr, uint16_t value) {
         (*this->memory)[addr] = value >> 8;
         (*this->memory)[addr+1] = value & 0x00FF;
     }
 
-    void Cpu::add16bRegisters(uint8_t& r1, uint8_t& r2, uint8_t& r3, uint8_t& r4) {
+    void Cpu::add16b2x2(uint8_t& r1, uint8_t& r2, uint8_t r3, uint8_t r4) {
         uint16_t result = this->joinBytes(r1, r2) + this->joinBytes(r3, r4);
         r1 = result >> 8;
         r2 = result & 0x00FF;
         this->setFlag(Flag::n, 0);
         this->setFlag(Flag::h, result == 0x0100);
         this->setFlag(Flag::cy, result == 0x0000);
+    }
+
+    uint8_t Cpu::jrConditional(Flag flag, bool value, int8_t jvalue) {
+        if (this->getFlag(flag) == value) {
+            this->PC += jvalue;
+            return 12;
+        }
+        return 8;
     }
 
     uint8_t Cpu::executeInstruction(bool cb, uint8_t opcode, uint8_t* args) {
@@ -126,15 +138,15 @@ namespace GBEMU {
                 return 8;
             // INC BC
             case 0x03:
-                this->inc16bRegister(this->B, this->C);
+                this->inc16b2(this->B, this->C);
                 return 8;
             // INC B
             case 0x04:
-                this->inc8bRegister(this->B);
+                this->inc8b(this->B);
                 return 4;
             // DEC B
             case 0x05:
-                this->dec8bRegister(this->B);
+                this->dec8b(this->B);
                 return 4;
             // LD B,u8
             case 0x06:
@@ -142,15 +154,15 @@ namespace GBEMU {
                 return 8;
             // RLCA
             case 0x07:
-                this->rlc8bRegister(this->A);
+                this->rlc8b(this->A);
                 return 4;
             // LD (u16),SP
             case 0x08:
-                this->mov16bRegister(this->joinBytes(args[0], args[1]), this->SP);
+                this->mov16bmem(this->joinBytes(args[0], args[1]), this->SP);
                 return 20;
             // ADD HL,BC
             case 0x09:
-                this->add16bRegisters(this->H, this->L, this->B, this->C);
+                this->add16b2x2(this->H, this->L, this->B, this->C);
                 return 8;
             // LD A,(BC)
             case 0x0A:
@@ -158,15 +170,15 @@ namespace GBEMU {
                 return 8;
             // DEC BC
             case 0x0B:
-                this->dec16bRegister(this->B, this->C);
+                this->dec16b2(this->B, this->C);
                 return 8;
             // INC C
             case 0x0C:
-                this->inc8bRegister(this->C);
+                this->inc8b(this->C);
                 return 4;
             // DEC C
             case 0x0D:
-                this->dec8bRegister(this->C);
+                this->dec8b(this->C);
                 return 4;
             // LD C,u8
             case 0x0E:
@@ -174,7 +186,7 @@ namespace GBEMU {
                 return 8;
             // RRCA
             case 0x0F:
-                this->rrc8bRegister(this->A);
+                this->rrc8b(this->A);
                 return 4;
             // STOP
             case 0x10:
@@ -191,15 +203,15 @@ namespace GBEMU {
                 return 8;
             // INC DE
             case 0x13:
-                this->inc16bRegister(this->D, this->E);
+                this->inc16b2(this->D, this->E);
                 return 8;
             // INC D
             case 0x14:
-                this->inc8bRegister(this->D);
+                this->inc8b(this->D);
                 return 4;
             // DEC D
             case 0x15:
-                this->dec8bRegister(this->D);
+                this->dec8b(this->D);
                 return 4;
             // LD D,u8
             case 0x16:
@@ -207,7 +219,7 @@ namespace GBEMU {
                 return 8;
             // RLA
             case 0x17:
-                this->rl8bRegister(this->A);
+                this->rl8b(this->A);
                 return 4;
             // JR i8
             case 0x18:
@@ -215,7 +227,7 @@ namespace GBEMU {
                 return 12;
             // ADD HL,DE
             case 0x19:
-                this->add16bRegisters(this->H, this->L, this->D, this->E);
+                this->add16b2x2(this->H, this->L, this->D, this->E);
                 return 8;
             // LD A,(DE)
             case 0x1A:
@@ -223,15 +235,15 @@ namespace GBEMU {
                 return 8;
             // DEC DE
             case 0x1B:
-                this->dec16bRegister(this->D, this->E);
+                this->dec16b2(this->D, this->E);
                 return 8;
             // INC E
             case 0x1C:
-                this->inc8bRegister(this->E);
+                this->inc8b(this->E);
                 return 4;
             // DEC E
             case 0x1D:
-                this->dec8bRegister(this->E);
+                this->dec8b(this->E);
                 return 4;
             // LD E,u8
             case 0x1E:
@@ -239,15 +251,11 @@ namespace GBEMU {
                 return 8;
             // RRA
             case 0x1F:
-                this->rr8bRegister(this->A);
+                this->rr8b(this->A);
                 return 4;
             // JR NZ,i8
             case 0x20:
-                if (!this->getFlag(Flag::zf)) {
-                    this->PC += (int8_t) args[0];
-                    return 12;
-                }
-                return 8;
+                return this->jrConditional(Flag::zf, 0, (int8_t) args[0]);  // 8 / 12
             // LD HL,u16
             case 0x21:
                 this->H = args[0];
@@ -256,19 +264,19 @@ namespace GBEMU {
             // LD (HL+),A
             case 0x22:
                 (*this->memory)[this->getHL()] = this->A;
-                this->inc16bRegister(this->H, this->L);
+                this->inc16b2(this->H, this->L);
                 return 8;
             // INC HL
             case 0x23:
-                this->inc16bRegister(this->H, this->L);
+                this->inc16b2(this->H, this->L);
                 return 8;
             // INC H
             case 0x24:
-                this->inc8bRegister(this->H);
+                this->inc8b(this->H);
                 return 4;
             // DEC H
             case 0x25:
-                this->dec8bRegister(this->H);
+                this->dec8b(this->H);
                 return 4;
             // LD H,u8
             case 0x26:
@@ -292,8 +300,105 @@ namespace GBEMU {
                 this->setFlag(Flag::zf, this->A == 0);
                 this->setFlag(Flag::h, 0);
                 return 4;
-            // // JR Z,i8
-            // case 0x28:
+            // JR Z,i8
+            case 0x28:
+                return this->jrConditional(Flag::zf, 1, (int8_t) args[0]);  // 8 / 12
+            // ADD HL,HL
+            case 0x29:
+                this->add16b2x2(this->H, this->L, this->H, this->L);
+                return 8;
+            // LD A,(HL+)
+            case 0x2A:
+                this->A = (*this->memory)[this->getHL()];
+                this->inc16b2(this->H, this->L);
+                return 8;
+            // DEC HL
+            case 0x2B:
+                this->dec16b2(this->H, this->L);
+                return 8;
+            // INC L
+            case 0x2C:
+                this->inc8b(this->L);
+                return 4;
+            // DEC L
+            case 0x2D:
+                this->dec8b(this->L);
+                return 4;
+            // LD L,u8
+            case 0x2E:
+                this->L = args[0];
+                return 8;
+            // CPL
+            case 0x2F:
+                this->A ^= 0xFF;
+                this->setFlags((uint8_t) Flag::n | (uint8_t) Flag::h);
+                return 4;
+            // JR NC,i8
+            case 0x30:
+                return this->jrConditional(Flag::cy, 0, (int8_t) args[0]);
+            // LD SP,u16
+            case 0x31:
+                this->SP = this->joinBytes(args[0], args[1]);
+                return 12;
+            // LD (HL-),A
+            case 0x32:
+                (*this->memory)[this->getHL()] = this->A;
+                this->dec16b2(this->H, this->L);
+                return 8;
+            // INC SP
+            case 0x33:
+                this->SP++;
+                return 8;
+            // INC (HL)
+            case 0x34:
+                this->inc8b((*this->memory)[this->getHL()]);
+                return 12;
+            // DEC (HL)
+            case 0x35:
+                this->dec8b((*this->memory)[this->getHL()]);
+                return 12;
+            // LD (HL),u8
+            case 0x36:
+                (*this->memory)[this->getHL()] = args[0];
+                return 12;
+            // SCF
+            case 0x37:
+                this->resetFlags((uint8_t) Flag::n | (uint8_t) Flag::h);
+                this->setFlag(Flag::cy, 1);
+                return 4;
+            // JR C,i8
+            case 0x38:
+                return this->jrConditional(Flag::cy, 1, (int8_t) args[0]);
+            // ADD HL,SP
+            case 0x39:
+                this->add16b2x2(this->H, this->L, (this->SP >> 8), (this->SP & 0x00FF));
+                return 8;
+            // LD A,(HL-)
+            case 0x3A:
+                this->A = (*this->memory)[this->getHL()];
+                this->dec16b2(this->H, this->L);
+                return 8;
+            // DEC SP
+            case 0x3B:
+                this->SP--;
+                return 8;
+            // INC A
+            case 0x3C:
+                this->inc8b(this->A);
+                return 4;
+            // DEC A
+            case 0x3D:
+                this->dec8b(this->A);
+                return 4;
+            // LD A,u8
+            case 0x3E:
+                this->A = args[0];
+                return 8;
+            // CCF
+            case 0x3F:
+                this->setFlag(Flag::cy, !this->getFlag(Flag::cy));
+                this->resetFlags((uint8_t) Flag::n | (uint8_t) Flag::h);
+                return 4;
             }
         }
 
