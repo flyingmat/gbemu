4fc8541dc5136389cd45624691e9a75b5262b4b9
flyingmat <paramattimatteo@gmail.com>
2020-04-10 03:26:28 +0100

    Restructured registers + more opcodes

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,9 +1,7 @@
 namespace GBEMU {
 
-    enum Side : bool {
-        right,
-        left
-    };
+    typedef unsigned char uchar;
+    typedef unsigned short int ushort;
 
     enum class Flag : unsigned char {
         zf = 0x01,
@@ -15,19 +13,31 @@ namespace GBEMU {
     class Cpu {
 
     private:
-        unsigned short int AF;
-        unsigned short int BC;
-        unsigned short int DE;
-        unsigned short int HL;
+        unsigned char A;
+        unsigned char F;
+        unsigned char B;
+        unsigned char C;
+        unsigned char D;
+        unsigned char E;
+        unsigned char H;
+        unsigned char L;
         unsigned short int SP;
         unsigned short int PC;
         unsigned char** memory;
 
+        unsigned short int joinBytes(unsigned char r1, unsigned char r2);
+        unsigned short int getBC();
         void setFlag(Flag flag, bool value);
-        void incSetZFH(unsigned char result);
-        void decSetZFH(unsigned char result);
-        void incHalfRegister(unsigned short int& r, Side side);
-        void decHalfRegister(unsigned short int& r, Side side);
+        void resetFlags(unsigned char flags);
+        void inc8bSetFlags(unsigned char result);
+        void dec8bSetFlags(unsigned char result);
+        void add16bSetFlags(unsigned short int result);
+        void inc8bRegister(unsigned char& r);
+        void dec8bRegister(unsigned char& r);
+        void rlc8bRegister(unsigned char& r);
+        void inc16bRegister(unsigned char& r1, unsigned char& r2);
+        void mov16bRegister(unsigned short int addr, unsigned short int value);
+        void add16bRegisters(unsigned char& r1, unsigned char& r2, unsigned char& r3, unsigned char& r4);
 
     public:
         Cpu(unsigned char*& memory);
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -2,11 +2,15 @@
 
 namespace GBEMU {
 
-    Cpu::Cpu(unsigned char*& memory) {
-        this->AF = 0;
-        this->BC = 0;
-        this->DE = 0;
-        this->HL = 0;
+    Cpu::Cpu(uchar*& memory) {
+        this->A = 0;
+        this->F = 0;
+        this->B = 0;
+        this->C = 0;
+        this->D = 0;
+        this->E = 0;
+        this->H = 0;
+        this->L = 0;
         this->SP = 0;
         this->PC = 0;
         this->memory = &memory;
@@ -16,46 +20,78 @@ namespace GBEMU {
         this->memory = nullptr;
     }
 
+    ushort Cpu::joinBytes(uchar r1, uchar r2) {
+        ushort out = r2;
+        return out | ((ushort) r1 << 8);
+    }
+
+    ushort Cpu::getBC() {
+        return this->joinBytes(this->B, this->C);
+    }
+
     void Cpu::setFlag(Flag flag, bool value) {
         if (value)
-            this->AF |= (unsigned char) flag;
+            this->F |= (uchar) flag;
         else
-            this->AF &= ((unsigned char) flag ^ 0xFF);
+            this->F &= ((uchar) flag ^ 0xFF);
     }
 
-    void Cpu::incSetZFH(unsigned char result) {
-        this->setFlag(Flag::h, result == 0x10);
+    void Cpu::resetFlags(uchar flags) {
+        this->F &= (flags ^ 0xFF);
+    }
+
+    void Cpu::inc8bSetFlags(uchar result) {
         this->setFlag(Flag::zf, result == 0x00);
+        this->setFlag(Flag::n, 0);
+        this->setFlag(Flag::h, result == 0x10);
     }
 
-    void Cpu::decSetZFH(unsigned char result) {
-        this->setFlag(Flag::h, result == 0x0F);
+    void Cpu::dec8bSetFlags(uchar result) {
         this->setFlag(Flag::zf, result == 0xFF);
+        this->setFlag(Flag::n, 1);
+        this->setFlag(Flag::h, result == 0x0F);
     }
 
-    void Cpu::incHalfRegister(unsigned short int& r, Side side) {
-        if (side == Side::right) {
-            r = (r & 0xFF00) | (((r & 0x00FF) + 1) & 0x00FF);
-            this->incSetZFH(r & 0x00FF);
-        }
-        else {
-            r = (r & 0x00FF) | (((r & 0xFF00) + 0x0100) & 0xFF00);
-            this->incSetZFH(r >> 8);
-        }
+    void Cpu::add16bSetFlags(ushort result) {
+        this->setFlag(Flag::n, 0);
+        this->setFlag(Flag::h, result == 0x0100);
+        this->setFlag(Flag::cy, result == 0x0000);
     }
 
-    void Cpu::decHalfRegister(unsigned short int& r, Side side) {
-        if (side == Side::right) {
-            r = (r & 0xFF00) | (((r & 0x00FF) - 1) & 0x00FF);
-            this->decSetZFH(r & 0x00FF);
-        }
-        else {
-            r = (r & 0x00FF) | (((r & 0xFF00) - 0x0100) & 0xFF00);
-            this->decSetZFH(r >> 8);
-        }
+    void Cpu::inc8bRegister(uchar& r) {
+        r++;
+        this->inc8bSetFlags(r);
     }
 
-    unsigned char Cpu::executeInstruction(bool cb, unsigned char opcode, unsigned char* args) {
+    void Cpu::dec8bRegister(uchar& r) {
+        r--;
+        this->dec8bSetFlags(r);
+    }
+
+    void Cpu::rlc8bRegister(uchar& r) {
+        this->setFlag(Flag::cy, r >> 7);
+        this->resetFlags((uchar) Flag::zf | (uchar) Flag::n | (uchar) Flag::h);
+    }
+
+    void Cpu::inc16bRegister(uchar& r1, uchar& r2) {
+        r2++;
+        if (!r2)
+            r1++;
+    }
+
+    void Cpu::mov16bRegister(ushort addr, ushort value) {
+        (*this->memory)[addr] = value >> 8;
+        (*this->memory)[addr+1] = value & 0x00FF;
+    }
+
+    void Cpu::add16bRegisters(uchar& r1, uchar& r2, uchar& r3, uchar& r4) {
+        ushort result = this->joinBytes(r1, r2) + this->joinBytes(r3, r4);
+        r1 = result >> 8;
+        r2 = result & 0x00FF;
+        this->add16bSetFlags(result);
+    }
+
+    uchar Cpu::executeInstruction(bool cb, uchar opcode, uchar* args) {
         if (!cb) {
             switch (opcode) {
             // NOP
@@ -63,23 +99,41 @@ namespace GBEMU {
                 return 4;
             // LD BC,u16
             case 0x01:
-                this->BC = (args[0] << 8) | args[1];
+                this->B = args[0];
+                this->C = args[1];
                 return 12;
             // LD (BC),A
             case 0x02:
-                (*this->memory)[this->BC] = (this->AF >> 8);
+                (*this->memory)[this->getBC()] = this->A;
                 return 8;
             // INC BC
             case 0x03:
-                this->BC++;
+                this->inc16bRegister(this->B, this->C);
                 return 8;
             // INC B
             case 0x04:
-                this->incHalfRegister(this->BC, Side::left);
+                this->inc8bRegister(this->B);
                 return 4;
+            // DEC B
             case 0x05:
-                this->decHalfRegister(this->BC, Side::left);
+                this->dec8bRegister(this->B);
                 return 4;
+            // LD B,u8
+            case 0x06:
+                this->B = args[0];
+                return 8;
+            // RLCA
+            case 0x07:
+                this->rlc8bRegister(this->A);
+                return 4;
+            // LD (u16),SP
+            case 0x08:
+                this->mov16bRegister(this->joinBytes(args[0], args[1]), this->SP);
+                return 20;
+            // ADD HL,BC
+            case 0x09:
+                this->add16bRegisters(this->H, this->L, this->B, this->C);
+                return 8;
             }
         }
 
